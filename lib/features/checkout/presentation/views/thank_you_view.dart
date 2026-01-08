08d7f5366de018152d5624e90939413860a424e1
Moaz-Omran20 <moazomran20@gmail.com>
2026-01-08 16:33:10 +0200

    custom container with checked background done

--- lib/features/checkout/presentation/views/thank_you_view.dart
+++ lib/features/checkout/presentation/views/thank_you_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment_integration/core/utils/app_styles.dart';
 
 class ThankYouView extends StatelessWidget {
   const ThankYouView({super.key});
@@ -14,7 +15,46 @@ class ThankYouView extends StatelessWidget {
           icon: Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(20),
+                ),
+                color: Color(0xFFD9D9D9),
+              ),
+            ),
+            Positioned(
+              top: -44,
+              right: 0,
+              left: 0,
+              child: CircleAvatar(
+                radius: 44,
+                backgroundColor: Color(0xFFD9D9D9),
+                child: CircleAvatar(
+                  radius: 33,
+                  backgroundColor: AppStyles.primaryColor,
+                  child: Icon(Icons.check, color: Colors.white, size: 40),
+                ),
+              ),
+            ),
+            Positioned(
+              left: -20,
+              bottom: MediaQuery.of(context).size.height * .2,
+              child: CircleAvatar(backgroundColor: Colors.white),
+            ),
+            Positioned(
+              right: -20,
+              bottom: MediaQuery.of(context).size.height * .2,
+              child: CircleAvatar(backgroundColor: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
