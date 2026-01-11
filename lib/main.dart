5cea697e0497f68299a4b8e07fb615b5bcb328e6
Moaz-Omran20 <moazomran20@gmail.com>
2026-01-11 23:39:51 +0200

    publishable key added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payment_integration/core/api_keys.dart';
 
 import 'features/checkout/presentation/views/my_cart_view.dart';
 
-void main() {
+void main() async {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const PaymentIntegration());
 }
-
 
 class PaymentIntegration extends StatelessWidget {
   const PaymentIntegration({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyCartView(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyCartView());
   }
 }
-
