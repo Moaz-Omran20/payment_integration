c9cab7f75f16c3ce53510cfc54bb17d654e52d2c
Moaz-Omran20 <moazomran20@gmail.com>
2026-01-17 23:38:50 +0200

    paypal payment method added

--- lib/core/shared_widgets/payment_methods_bottom_sheet.dart
+++ lib/core/shared_widgets/payment_methods_bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
+import 'package:payment_integration/core/api_keys.dart';
 import 'package:payment_integration/core/models/payment_intent_input_model.dart';
 import 'package:payment_integration/core/shared_widgets/payment_methods_list_view.dart';
 import 'package:payment_integration/features/checkout/presentation/view_model/payment_cubit.dart';
@@ -8,19 +10,17 @@ import 'package:payment_integration/features/checkout/presentation/views/thank_y
 import 'custom_button.dart';
 
 class PaymentMethodsBottomSheet extends StatefulWidget {
-
-
   const PaymentMethodsBottomSheet({super.key});
+
   @override
-  State<PaymentMethodsBottomSheet> createState() => _PaymentMethodsBottomSheetState();
+  State<PaymentMethodsBottomSheet> createState() =>
+      _PaymentMethodsBottomSheetState();
 }
 
 class _PaymentMethodsBottomSheetState extends State<PaymentMethodsBottomSheet> {
-  int selectedPaymentIndex =0;
+  int selectedPaymentIndex = 0;
 
-
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15),
@@ -29,9 +29,15 @@ class _PaymentMethodsBottomSheetState extends State<PaymentMethodsBottomSheet> {
         children: [
           Row(
             mainAxisSize: MainAxisSize.max,
-            children: [Expanded(child: PaymentMethodsListView(onChanged: (value) {
-               selectedPaymentIndex = value;
-            },))],
+            children: [
+              Expanded(
+                child: PaymentMethodsListView(
+                  onChanged: (value) {
+                    selectedPaymentIndex = value;
+                  },
+                ),
+              ),
+            ],
           ),
           SizedBox(height: 35),
           BlocConsumer<PaymentCubit, PaymentState>(
@@ -50,14 +56,82 @@ class _PaymentMethodsBottomSheetState extends State<PaymentMethodsBottomSheet> {
                 return CustomButton(
                   title: "Continue",
                   onTap: () {
+                    selectedPaymentIndex == 0
+                        ? context.read<PaymentCubit>().makePayment(
+                            paymentIntentInputModel: PaymentIntentInputModel(
+                              amount: "2000",
+                              currency: "usd",
+                              // customerId: "cus_TmL4M1Q4i64922",
+                            ),
+                          )
+                        : Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => PaypalCheckoutView(
+                                sandboxMode: true,
+                                clientId: ApiKeys.paypalClientId,
+                                secretKey: ApiKeys.paypalSecretKey,
+                                transactions: const [
+                                  {
+                                    "amount": {
+                                      "total": '70',
+                                      "currency": "USD",
+                                      "details": {
+                                        "subtotal": '70',
+                                        "shipping": '0',
+                                        "shipping_discount": 0,
+                                      },
+                                    },
+                                    "description":
+                                        "The payment transaction description.",
+                                    // "payment_options": {
+                                    //   "allowed_payment_method":
+                                    //       "INSTANT_FUNDING_SOURCE"
+                                    // },
+                                    "item_list": {
+                                      "items": [
+                                        {
+                                          "name": "Apple",
+                                          "quantity": 4,
+                                          "price": '5',
+                                          "currency": "USD",
+                                        },
+                                        {
+                                          "name": "Pineapple",
+                                          "quantity": 5,
+                                          "price": '10',
+                                          "currency": "USD",
+                                        },
+                                      ],
 
-                    selectedPaymentIndex ==0?  context.read<PaymentCubit>().makePayment(
-                      paymentIntentInputModel: PaymentIntentInputModel(
-                        amount: "2000",
-                        currency: "usd",
-                        // customerId: "cus_TmL4M1Q4i64922",
-                      ),
-                    ):null;
+                                      // shipping address is not required though
+                                      //   "shipping_address": {
+                                      //     "recipient_name": "tharwat",
+                                      //     "line1": "Alexandria",
+                                      //     "line2": "",
+                                      //     "city": "Alexandria",
+                                      //     "country_code": "EG",
+                                      //     "postal_code": "21505",
+                                      //     "phone": "+00000000",
+                                      //     "state": "Alexandria"
+                                      //  },
+                                    },
+                                  },
+                                ],
+                                note:
+                                    "Contact us for any questions on your order.",
+                                onSuccess: (Map params) async {
+                                  print("onSuccess: $params");
+                                },
+                                onError: (error) {
+                                  print("onError: $error");
+                                  Navigator.pop(context);
+                                },
+                                onCancel: () {
+                                  print('cancelled:');
+                                },
+                              ),
+                            ),
+                          );
                   },
                 );
               }
@@ -66,4 +140,5 @@ class _PaymentMethodsBottomSheetState extends State<PaymentMethodsBottomSheet> {
         ],
       ),
     );
-  }}
+  }
+}
